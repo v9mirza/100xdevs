9cc599acfbdf53a6b54e224a4cb3347ac5e5f693
mirza141x <mirza141x@gmail.com>
2025-02-17 23:03:40 +0530

    added week-6

---
+++ web-dev/week-6/auth/index.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const users = [];
+
+function generateToken() {
+    let options = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    let token = "";
+    for (let i = 0; i < 32; i++) {
+        token += options[Math.floor(Math.random() * options.length)];
+    }
+    return token;
+}
+
+app.post("/signup", (req, res) => {
+    // Check if username and password exist in request
+    if (!req.body.username || !req.body.password) {
+        res.status(400).json({
+            message: "Username and password are required"
+        });
+        return;
+    }
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username.length < 5) {
+        res.json({
+            message: "Username must be at least 5 characters"
+        });
+        return;
+    }
+
+    users.push({ username, password });
+    res.json({
+        message: "You have signed up successfully"
+    });
+});
+
+console.log(users);
+
+
+app.post("/signin", (req, res) => {
+    // Check if username and password exist in request
+    if (!req.body.username || !req.body.password) {
+        res.status(400).json({
+            message: "Username and password are required"
+        });
+        return;
+    }
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = users.find(user => user.username === username && user.password === password);
+    
+    if (user) {
+        const token = generateToken();
+        user.token = token;
+        res.json({ token });
+        console.log(users);
+    } else {
+        res.status(403).json({
+            message: "Invalid username or password"
+        });
+    }
+    console.log(users);
+});
+
+
+app.get("/me", (req, res) => {
+    const token = req.headers.authorization;
+    const user = users.find(user => user.token === token);
+    if (user) {
+        res.send({
+            username: user.username
+        })
+    } else {
+        res.status(401).send({
+            message: "Unauthorized"
+        })
+    }
+})
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
